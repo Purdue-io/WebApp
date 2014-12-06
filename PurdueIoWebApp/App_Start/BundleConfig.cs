e6a796b921bb238120782971b81550fd1dbbc802
Hayden <hayden@outlook.com>
2014-12-06 02:46:41 -0500

    A simple starting template.

---
+++ PurdueIoWebApp/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace PurdueIoWebApp
+{
+	public class BundleConfig
+	{
+		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			bundles.Add(new ScriptBundle("~/bundles/typescript").Include(
+						"~/Scripts/typescript/"));
+
+
+
+			bundles.Add(new StyleBundle("~/bundles/css").Include(
+					  "~/Content/css/App.css"));
+		}
+	}
+}
---
+++ PurdueIoWebApp/Scripts/typescript/App.js
@@ -0,0 +1,5 @@
+﻿var App = (function () {
+    function App() {
+    }
+    return App;
+})();
